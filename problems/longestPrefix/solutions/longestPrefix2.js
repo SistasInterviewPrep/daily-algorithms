9b74feaed94aaa1498d1cbd217da212012018b80
Aditi Green <aditigreen@aditis-mbp.home>
2018-07-06 01:37:13 -0400

    solved algorithm using jankiest code ever

--- problems/longestPrefix/solutions/longestPrefix2.js
+++ problems/longestPrefix/solutions/longestPrefix2.js
@@ -1,11 +1,29 @@
 function LongestPrefix (array){
-        var dup = {};
-        let splitWord = array.map(word => word.split(""))
-        let flattenArr = splitWord.reduce( (a,b) => a.concat(b) )
-          flattenArr.forEach( (letter,index)=>{
-              dup.hasOwnProperty(letter) ? dup[letter]++ : dup[letter] = 1
-          })
-      
-      }
-      
-      LongestPrefix(["flower","flow","flight"])
+    var dup = {};
+    let splitWord = array.map(word => word.split(""))
+    let flattenArr = splitWord.reduce( (a,b) => a.concat(b) )
+      flattenArr.forEach( (letter,index)=>{
+          dup.hasOwnProperty(letter) ? dup[letter]++ : dup[letter] = 1
+      })
+console.log(dup)
+  for(let key in dup ){
+       for(let j in dup){
+            if(dup[key] < dup[j] && dup[j] === array.length){
+              delete dup[key]
+          }
+          else{
+            continue
+          }
+       }
+        
+  }
+ let prefix =  Object.keys(dup).join("")
+  array.forEach( word => {
+    if(word.indexOf(prefix) < 0){
+      prefix = ""
+    }
+  })
+  return prefix
+}
+  
+  LongestPrefix(["flower","flow","flight"])
